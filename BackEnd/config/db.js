07185d1448c38d6abd5cfdc2863f3fceb0955044
Faishal <faishalh994@gmail.com>
2026-02-15 12:38:22 +0530

    Connected MongoDB Atlas and updated server configuration

--- BackEnd/config/db.js
+++ BackEnd/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/taxpal");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected");
   } catch (error) {
     console.error("DB Error:", error);
